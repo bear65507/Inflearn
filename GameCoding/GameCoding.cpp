cf5ac70b1485b695870e2f6e981fd8042bc2408b
unknown <bear65507@gmail.com>
2025-04-06 23:00:30 +0900

    섹션 7. 선형 자료구조 - 트리 기초

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -1,10 +1,107 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
+class Node
+{
+public:
+	Node(const char* data) : data(data) {}
+public:
+	const char* data;
+	vector<Node*> children;
+};
 
+Node* CreateTree()
+{
+	Node* root = new Node("R1 개발실");
+	{
+		Node* node = new Node("디자인팀");
+		root->children.push_back(node);
+		{
+			Node* leaf = new Node("전투");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("경제");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("스토리");
+			node->children.push_back(leaf);
+		}
+	}
+	{
+		Node* node = new Node("프로그래밍");
+		root->children.push_back(node);
+		{
+			Node* leaf = new Node("클라");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("서버");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("엔진");
+			node->children.push_back(leaf);
+		}
+	}
+	{
+		Node* node = new Node("아트");
+		root->children.push_back(node);
+		{
+			Node* leaf = new Node("배경");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("캐릭터");
+			node->children.push_back(leaf);
+		}
+	}
 
+	return root; 
+}
+
+void PrintTree(Node* root, int depth = 0)
+{
+	for (int i = 0; i < depth; i++)
+		cout << "-";
+
+	cout << root->data << endl;
+
+	int size = root->children.size();
+	if (size == 0)
+		return;
+	for (int i = 0; i < size; i++)
+	{
+		Node* node = root->children[i];
+		PrintTree(node);
+	}
+}
+
+int GetHeight(Node* root)
+{
+	int ret = 1;
+	int size = root->children.size();
+	for (int i = 0; i < size; i++)
+	{
+		Node* node = root->children[i];
+		int h = GetHeight(node) + 1;
+
+		if (ret < h)
+			ret = h;
+	}
+
+	return ret;
+}
 
 int main()
 {
-	
+	Node* root = CreateTree();
+
+	PrintTree(root);
+
+	int h = GetHeight(root);
+
+	return 0;
 }
