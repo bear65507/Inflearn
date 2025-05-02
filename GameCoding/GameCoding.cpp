48d6b73fdf8e1078d365103173e972da8fceba59
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-02 18:48:34 +0900

    섹션 8. 트리와 그래프 - BFS

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -1,10 +1,12 @@
 ﻿#include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 // DFS (Depth First Search) == 시작점으로 기준으로 얼마나 깊이 들어가는가
 // DFS = 재귀함수, 트리와 다르게 양방향으로 이동할 수 있음을 고려 = 또는 스택으로 구현
-// 
+// BFS (Breath First Search) 너비
+// BFS = 큐
 
 struct Vertex
 {
@@ -14,8 +16,11 @@ struct Vertex
 vector<Vertex> vertices;
 vector<vector<int>> adjacent;
 
-// 내가 방문한 목록
+// 내가 방문한 목록(DFS)
 vector<bool> visited;
+
+// 내가 발견한 목록(BFS)
+vector<bool> discovered;
 
 void CreateGraph()
 {
@@ -43,56 +48,79 @@ void CreateGraph()
 	};
 }
 
-void Dfs(int here) // here == 시작점
+// BFS의 시간복잡도는 DFS와 동일
+void Bfs(int here) // 가장 먼저 발견한 것을 먼저 방문, 선입선출과 비슷
 {
-	// 방문 도장 찍기
-	visited[here] = true;
-	cout << "Visited : " << here << endl;
+	// ex) 누구에 의해서 발견되었는지?
+	vector<int> parent(6, -1);
+	// ex) 시작점에서 얼만큼 떨어져 있는지?
+	vector<int> dist(6, -1);
 
-	/* 인접한 길을 다 체크해서 <인접 리스트 버전> 
-	* 시간복잡도는 O(V+E) [V는 정점, E는 간선 개수]
-	* (정점마다 한번씩 호출 V) + (간선의 총 개수 E개)) == V+E
-	const int size = adjacent[here].size();
-	for (int i = 0; i < size; i++)
+
+	queue<int> q;
+	// 예약 시스템 : 데이터 한 번 넣음
+	q.push(here);
+	discovered[here] = true;
+	// 시작점으로 초기화
+	parent[here] = here;
+	dist[here] = 0;
+
+	while (q.empty() == false) // 스캔이 다 끝날 때까지
 	{
-		int there = adjacent[here][i];
-		if (visited[there] == false) // 방문하지 않은 곳만 DFS
-			Dfs(there); 
-	}
-	*/
+		// 큐가 비워질 때까지 데이터 꺼냄
+		here = q.front();
+		q.pop();
 
-	// <인접 행렬 버전> 알고리즘은 같으나 표현 방법만 달라짐
-	// 정점 개수가 V일 때, 시간복잡도는 O(V**2)
-	// (정점마다 V번 Dfs()함수 호출) * (for문을 V번 만큼 순회) == V**2
-	for (int there = 0; there < 6; there++)
-	{
-		// 길은 있는지
-		if (adjacent[here][there] == 0)
-			continue;
+		// 방문 도장 찍기
+		cout << "Visited : " << here << endl;
 
-		// 아직 방문하지 않은 곳에 한해서 방문
-		if (visited[there] == false)
-			Dfs(there);
+		// 인접한 정점 탐색
+		// 인접 리스트 버전
+		/*int size = adjacent[here].size();
+		for (int i = 0; i < size; i++)
+		{
+			int there = adjacent[here][i];
+			if (discovered[here]) // 이미 다른 경로에서 발견했다면 무시
+				continue;
+
+			// 방문 예약 (큐의 특징)
+			q.push(there);
+			discovered[there] = true;
+		} */
+		// 인접 행렬 버전
+		for (int there = 0; there < 6; there++)
+		{
+			if (adjacent[here][there] == 0) // 주위 정점이 없다면 무시
+				continue;
+			if (discovered[there]) // 이미 다른 경로에서 발견했다면 무시
+				continue;
+			
+			// 방문 예약
+			q.push(there);
+			discovered[there] = true;
+			//
+			parent[there] = here;
+			dist[there] = dist[here] + 1; // 이전 좌표에서 1을 더함
+		}
 	}
 }
 
-void DfsAll() // 모든 정점들을 다 순회
+void BfsAll()
 {
-	visited = vector<bool>(6, false);
+	discovered = vector<bool>(6, false);
 
 	for (int i = 0; i < 6; i++)
-		if (visited[i] == false)
-			Dfs(i);
+		if (discovered[i] == false)
+			Bfs(i);
 }
 
 int main()
 {
 	CreateGraph();
-	
-	//visited = vector<bool>(6, false);
-	//Dfs(0);
 
-	DfsAll();
+	discovered = vector<bool>(6, false);
+	Bfs(0);
 
+	//BfsAll();
 	return 0;
 }
