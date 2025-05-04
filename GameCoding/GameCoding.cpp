4618c47772e3e4257c43453e9e34f92a967d0484
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-04 12:26:45 +0900

    섹션 9. STL - 함수 포인터

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -3,101 +3,103 @@
 #include <queue>
 using namespace std;
 
-// 다익스트라 = BFS + 가중치(작을수록 우선순위 높다)
-// 예약 시스템에서 등록한 모든 애들 중에서 점수가 가장 좋은 애 먼저 꺼냄
-// 다익스트라 = 우선순위 큐
-
-struct Vertex
+void Print()
 {
-	// int data;
-};
-
-vector<Vertex> vertices;
-vector<vector<int>> adjacent;
-
-void CreateGraph()
-{
-	vertices.resize(6);
-	adjacent = vector<vector<int>>(6, vector<int>(6, -1));
-	adjacent[0][1] = adjacent[1][0] = 15;
-	adjacent[0][3] = adjacent[3][0] = 35;
-	adjacent[1][2] = adjacent[2][1] = 5;
-	adjacent[1][3] = adjacent[3][1] = 10;
-	adjacent[3][4] = adjacent[4][3] = 5;
-	adjacent[5][4] = adjacent[4][5] = 5;
+	cout << "Hello, World!" << endl;
 }
 
-struct VertexCost
+int Add(int a, int b)
 {
-	VertexCost(int cost, int vertex) : cost(cost), vertex(vertex) { }
+	return a + b;
+}
 
-	bool operator<(const VertexCost& other) const
-	{
-		return cost < other.cost;
-	}
-	bool operator>(const VertexCost& other) const
-	{
-		return cost > other.cost;
-	}
+// 콜백 함수 : 어떤 행동을 했을 때 역으로 함수를 호출해 주는 것
+// ex) UI : 어떤 버튼을 눌렀을 때 함수 호출 / ex) 키보드 입력 
 
-	int cost;
-	int vertex;
+// '행동' 자체를 인자로 넘기고 싶을 때
+using FuncPtrType = int(*)(int a, int b);
+
+int DoSomething(int a, int b, FuncPtrType func)
+{
+	return func(a, b);
+}
+
+class Item
+{
+public:
+
+public:
+	int _itemid = 0;
+	int _rarity = 0;
+	int _ownerid = 0;
 };
 
-void Dijikstra(int here)
+/*  -------------------------------------------------------------------------------- */
+using ItemSelectorType = bool(*)(Item* item);
+
+// 각 조건마다 함수 자체를 넣어 처리하면 코드가 너무 방대해짐 --> 함수 포인터 사용
+Item* FindItem(Item items[], int itemCount, ItemSelectorType selector)
 {
-	priority_queue<VertexCost, vector<VertexCost>, greater<VertexCost>> pq;
-	vector<int> best(6, INT32_MAX);
-	vector<int> parent(6, -1);
-
-	pq.push(VertexCost(0, here));
-	best[here] = 0;
-	parent[here] = here;
-
-	while (pq.empty() == false)
+	for (int i = 0; i < itemCount; i++) // 모든 아이템들을 순회
 	{
-		// 제일 좋은 후보를 찾는다
-		VertexCost v = pq.top();
-		pq.pop();
-
-		int cost = v.cost;
-		here = v.vertex;
-
-		// 더 짧은 경로를 뒤늦게 찾았다면 스킵
-		if (best[here] < cost)
-			continue;
-
-		// 방문
-		cout << "방문!" << here << endl;
-
-		// 인접 행렬 방식
-		for (int there = 0; there < 6; there++)
-		{
-			// 연결되지 않았다면 스킵
-			if (adjacent[here][there] == -1)
-				continue;
-
-			// 지금까지 가려는 제일 좋은 비용 + 추가적으로 가야될 비용 
-			int nextCost = best[here] + adjacent[here][there]; 
-			// 더 좋은 경로를 과거에 찾았으면 스킵
-			if (nextCost >= best[there])
-				continue;
-
-			// 지금까지 찾은 경로중에서는 최선의 수치 = 갱신
-			best[there] = nextCost;
-			parent[there] = here; // 나중에 갱신될 수 있음
-			pq.push(VertexCost(nextCost, there));
-				
-		}
+		Item* item = &items[i];
+		if (selector(item)) // IsRare(item)
+			return item;
 	}
 
+	return nullptr;
 }
+
+bool IsRare(Item* item)
+{
+	return item->_rarity == 1;
+}
+/* ----------------------------------------------------------------------------------- */
+// 멤버 함수 포인터
+class Test
+{
+public:
+	void PrintTest(){ }
+};
+
 
 int main()
 {
-	CreateGraph();
+	using DataType = int;
 
-	Dijikstra(0);
+	/* 잘 안 쓰이는 문법 (쓰임새 없음)
+	using FuncType = void(); // = void를 입력으로 받는 함수를 FuncType으로 부르겠다(시그니쳐)
+	FuncType* ptr = &Print; // 함수 포인터, 주소값 변수(&)는 생략가능
+	ptr(); // 함수의 주소를 타고 가서 함수를 실행
+
+	using FuncType2 = int(int, int); // int 타입을 반환하고 int 2개를 입력으로 받는 함수를
+								  // FuncType2로 부르겠다(시그니쳐)
+	FuncType2* ptr2 = &Add;
+	int ret = ptr2(10, 20); // 시그니쳐에 맞추기
+	*/
+
+	using FuncPtrType = void(*)(); // void를 뱉어주고 void를 받아주는 함수의 포인터타입인
+								// FuncPtrType을 선언 (현대 문법)
+	FuncPtrType ptr = &Print;
+	ptr();
+
+	DoSomething(10, 20, &Add);
+	DoSomething(10, 20, [](int a, int b) { return a - b; }); // 람다 함수 (익명 함수)
+
+	Item items[10];
+	items[3]._rarity = 1; 
+	FindItem(items, 10, IsRare); // rarity가 1인 아이템 찾기
+
+	// 멤버함수 포인터 (정적/전역 함수랑 다르다) --> 기존 방법으론 사용 불가
+	// 함수 호출 규약이 다름
+	// (함수를 호출할 때 전달되는 인자들의 순서나 함수 종료 시 누가 함수를 정리하는 지)
+	using MemFuncPtrType = void(Test::*)();
+	MemFuncPtrType funcPtr = &Test::PrintTest;
+	
+	Test t;
+	(t.*funcPtr)();
+	Test* t2 = &t;
+	(t2->PrintTest)();
 
 	return 0;
 }
