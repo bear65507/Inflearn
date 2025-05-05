6ff5a308bea50865fb3706d8d3c94a274ee205cc
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-05 19:07:07 +0900

    섹션 9. STL - vector

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -3,101 +3,110 @@
 #include <queue>
 using namespace std;
 
-// 함수 객체
-// 함수 포인터의 단점
-// - 시그니쳐가 안 맞으면 사용 불가
-// - 상태(데이터)를 가질 수 없음(데이터랑 바인딩 불가)
-
-class Functor
+void Test(vector<int>& v) // 참조에 의한 전달 (원본 값)
 {
-public:
-	void operator() () // 괄호에 대한 오퍼레이터 오버로딩
-	{
-		cout << "Functor Test" << endl;
-		cout << _value << endl;
-	}
 
-	void operator()(int n) // 오퍼레이터 오버로딩으로 시그니쳐 호환 문제 해결
-	{
-		cout << "Functor Test" << endl;
-		cout << _value + n << endl;
-	}
-public:
-	int _value = 0;
-};
-
-/* ------------------------------------------------------------ */
-struct AddStruct
-{
-public:
-	int operator()(int a, int b)
-	{
-		return a + b;
-	}
-};
-
-template<typename T> // functor와 궁합이 좋다
-int DoSomething(int a, int b, T func)
-{
-	return func(a, b);
 }
-
-template<typename T>
-struct Greater
-{
-	bool operator()(const T& left, const T& right) const
-	{
-		return left > right;
-	}
-};
-
-/* ------------------------------------------------------------- */
-
-class Job
-{
-public:
-	Job() { }
-	virtual ~Job() { }
-};
-
-class MoveJob : public Job
-{
-public:
-	MoveJob(int x, int y) : x(x), y(y) { }
-
-	void operator()()
-	{
-		cout << "player move" << endl;
-	}
-public:
-	int x;
-	int y;
-};
-
-class AttackJob : public Job
-{
-
-};
 
 int main()
 {
-	Functor func;
-	func._value = 10;
-	func(); // 어떤 상태를 저장한 다음에 어떤 동작까지 연이어 실행
-	func(10);
+	vector<int> v{ 1, 2, 3, 4, 5 };
+	// size (resize) == 실제 데이터 크기
+	// capacity (reserve) == 실제로 사용할(할당된) 공간
+	// 삽입/삭제
+	// - 시작   O(N)
+	// - 중간   O(N)
+	// - 끝     O(1)
+	// push_back, front, back
+	// 임의 접근 v[i]   O(1)
 
-	AddStruct func;
-	DoSomething(10, 20, func);
+	int a = v.front(); // O(1)
+	int b = v.back();  // O(1)
 
-	priority_queue<int, vector<int>, Greater<int>> pq;
+	// 순회
+	const int size = v.size(); // size()함수를 반복적으로 호출하는 비용을 아끼고 싶을 때
+	for (int i = 0; i < size; i++)
+	{
+		cout << v[i] << endl;
+	}
 
-	// 클라이언트 : (10, 20) 좌표로 이동할래
-	MoveJob* job = new MoveJob(10, 20);
-	AttackJob* attackjob = new AttackJob();
-	(*job)();
+	// 초기화
+	vector<int> v2(5); // 데이터 개수
+	vector<int> v3(5, -1); // 데이터 개수, 초기값
+	vector<int> v4{ 1, 2, 3, 4, 5 }; // 원하는 데이터로 채움
 
-	queue<Job>;
+	// 벡터라는 클래스 자체는 stack에 있지만 실제 데이터는 heap 영역에 들어가 있음
+	vector<int> v5 = v4;
+	v5[0] = 100; // 얕은(간접적인) 복사
 
+	// clear()
+	v.clear();
+	cout << v.size() << endl; // size는 0
+	cout << v.capacity() << endl; // capacity는 변하지 않음
+
+	// iterator : 반복자, 순회자 (포인터 개념)
+	{
+		vector<int> v{ 1, 2, 3, 4, 5 };
+		int* ptr = &v[0]; // 첫번째 데이터에 접근
+		int value = *ptr; // 데이터 추출
+
+		ptr++; // 다음 데이터로 넘어가기
+		ptr += 2; // 다다음 데이터로 넘어가기
+		ptr = &v[0];
+
+		int* ptrEnd = &v[4] + 1; // 마지막 데이터 다음을 가리키도록 유도
+		while (ptr != ptrEnd) // 모든 데이터 순회
+		{
+			cout << *ptr << endl;
+			ptr++;
+		}
+	}
+
+	// STL 제공 순회자 사용
+	{
+		vector<int> v{ 1,2,3,4,5 };
+
+		vector<int>::iterator it = v.begin();
+		vector<int>::iterator itEnd = v.end();
+
+		for (it; it != itEnd; it++)
+		{
+			int data = *it;
+			if (data == 3) // 벡터 안에 3이 있는지?
+			{
+				// 찾음
+				break;
+			}
+			cout << data << endl;
+		}
+
+		if (it == v.end())
+		{
+			// 데이터 못찾음
+		}
+
+		// 데이터 삭제
+		if (it != v.end())
+		{
+			v.erase(it); // 3 삭제
+		}
+	}
+
+	// 모든 데이터들을 순회하며 특정 조건에 따라서 실시간으로 삭제
+	{
+		vector<int> v{ 1,2,3,4,5 };
+		vector<int>::iterator it;
+		for (it = v.begin(); it != v.end(); it) // it++하면 안됨
+		{
+			// 짝수인 데이터 삭제
+			int value = *it;
+			if (value % 2 == 0)
+				it = v.erase(it); // 삭제된 위치의 순회자를 다음 위치로 갱신
+			else 
+				// 순회를 하며 삭제하는 경우, erase()할 때와 아닐때를 구분해서 만들어야 함
+				it++; 
+		}
+	}
 
 	return 0;
 }
