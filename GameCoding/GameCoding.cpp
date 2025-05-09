36a8c94459b651ec0368992bbd7132b2aaa318d5
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-09 16:40:32 +0900

    섹션 9. STL - hash_map

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -1,88 +1,61 @@
 ﻿#include <iostream>
 #include <vector>
 #include <map>
-#include <set>
+#include <unordered_map>
 using namespace std;
 
-class Player
-{
-public:
-	Player() {}
-	Player(int id) : _id(id) {}
+// (☆)메모리를 팔아서 (CPU)성능을 얻겠다
 
+// ex) 아파트 우편함
+// [201][202][203][204]
+// [101][102][103][104]
+// 203호 우편 --> 바로 접근 = O(1)
 
-	int _id = 0;
-};
+// 키를 알면 빠르게 찾을 수 있다 O(1)
+// -> hash 기법
+// 보안 업계에서 주로 사용, 비밀번호를 해시 알고리즘으로 암호화해서 저장,
+// 해시값만 알고는 비밀번호를 알 수 없다(단방향)
+// == 단방향으로 유니크한 키를 꺼내는 기법
 
-template<typename T, typename U>
-struct Pair // std::pair를 직접 구현
-{
-	T first;
-	U second;
-};
-
-template<typename T, typename U> // std::make_pair를 직접 구현
-auto MakePair(T first, U second) // 템플릿 문법과 auto를 활용한 형식 연역
-{
-	return std::pair<T, U>(first, second);
-}
+// 복잡한 값이라도 해시를 취해서 키값을 추출한다음 특정 칸(버킷)에 데이터를 넣어준다
+// ex) 아이디 % 10000 = 키
+// [1][2][3]...[10000]
+// hash(100002324024120002) -> 2 
+// -> 2번째 칸(버킷)에 저장
 
 
 int main()
 {
-	vector<Player*> v;
+	// hash_map
+	// 탐색의 시간복잡도 : O(1)
+	unordered_map<int, int> um;
 
-	// 벡터에 객체를 넣어줌
-	// 객체 자체를 넣기 보단 동적할당 해서 포인터로 들고 있게 하는게 좋음
-	// --> 이동, 복사 비용을 아낌
-	v.push_back(new Player(100));
-	v.push_back(new Player(200));
-	v.push_back(new Player(300));
-	v.push_back(new Player(400));
-	v.push_back(new Player(500));
+	// 삽입/삭제, 탐색, 순회가 map과 비슷
+	// 삽입
+	um.insert(make_pair(10, 100));
+	um[20] = 200;
 
-
-	// 벡터에는 여러 한계가 있음, 따라서 (키, 값) 형태로 데이터를 저장하는 맵 사용
-	map<int, Player*> m; // key를 int, value를 Player타입으로 저장
-
-	// 데이터 추가 시 정렬되어 들어감 (레드-블랙 트리 처럼 균형 잡히게)
-	/* 데이터 추가, 키와 값을 짝을 이뤄서 */
-	for (Player* player : v)
+	// 찾기
+	auto findit = um.find(10);
+	if (findit != um.end())
 	{
-		m.insert(make_pair(player->_id, player));
-		//m[player->_id] = player; // 바로 넣어주는 방법
-	}
-
-    /* 데이터 찾기, 정렬되어 있는 구조이기에 빠름(O(log N), 이진탐색과 비슷) */
-	auto it = m.find(300); // 300을 찾고 순회자를 반환
-
-	if (it != m.end()) // 데이터를 찾음
-	{
-		int key = it->first;
-		Player* value = it->second;
 		cout << "찾음" << endl;
 	}
-	else // 데이터 없음
+	else
 	{
 		cout << "없음" << endl;
 	}
-	Player* p = m[100]; // 키 값으로 접근해서 값을 꺼내옴
-					  // 없는 키값으로 접근하면? C++에서는 없으면 기본값으로 추가
 
-	/* 데이터 삭제 */
-	m.erase(200);
+	// 삭제
+	um.erase(10);
+	//um.erase(findit);
 
-	m.erase(it); // 순회자를 인자로 받는 버전
-
-	/* 데이터 순회, 순회는 벡터가 더 유리함 */
-	for (auto it = m.begin(); it != m.end(); it++)
+	// 순회
+	for (auto it = um.begin(); it != um.end(); it++)
 	{
 		int key = it->first;
-		Player *p = it->second;
+		int value = it->second;
 	}
-
-	// C++ vector = C# List
-	// C++ hash_map = C# Dictionary
 
 	return 0;
 }
