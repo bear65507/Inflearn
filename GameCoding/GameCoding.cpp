e9cc2604253cc20e5b6371209baade265df0744d
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-12 19:50:01 +0900

    섹션 10. 알고리즘과 모던 C++ - 멀티바이트와 유니코드

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -5,55 +5,46 @@ using namespace std;
 
 int main()
 {
-	// C스타일 문자열
-	// 문자열이 시작하는 위치를 포인터로 들고 있고, null문자로 끝임을 알림
-	char name[] = "Rookiss";
-	char name2[] = "Rookiss";
+	// 1) ASCII 코드 -> 영어만 고려
+	// 문자 표현 7bit (0~127)
+	char ch = 'A';
 
-	if (name == name2) // 두 문자열의 주소값을 비교
-	{
-		cout << "같음" << endl; 
-	}
-	else
-	{
-		cout << "다름" << endl;
-	}
-	// 다루기가 불편 (strcmp, strcat...)
+	// 2) ANSI
+	// ASCII + 각국 언어별로 바이트를 늘려 표현
+	// 127번까지는 ASCII, 그 이후부터는 새로운 페이지를 정함
+	// ex) CP949, EUC-KR
+	// 로케일 환경에 따라 문자가 깨질 수도 있음 (동일한 키코드 != 동일한 문자)
+	
+	setlocale(LC_ALL, "");
+	cout << "LC_ALL: " << setlocale(LC_ALL, NULL) << endl;
 
-	// string 문자열
-	std::string str = "Rookiss";
-	std::string str2 = "Rookiss";
+	// 3) 유니코드
+	// 동일 번호 = 동일한 문자 = 동일한 유니코드
+	// 인코딩 방식 (대표적으로 UTF-8, UTF-16)
+	// - UTF8 : 영어(1바이트), 한국어/중국어...(3바이트)'
+	// - UTF16 : 영어(2), 한국어(2), 중국어(2) ...
 
-	// 1) 비교
-	// ex) ID/PW 맞는지
-	if (str == str2)
-	{
-		// TODO
-	}
+	// 4) MBCS(Multi Byte Character Set) vs WBCS(Wide Byte Character Set)
+	// - 멀티바이트 집합(MBCS) : 가변 길이 인코딩 (CP949, UTF-8)
+	// - 유니코드 집합(WBCS) : 고정길이 인코딩 (UTF-16)
 
-	// 2) 복사
-	string str3;
-	str3 = str; // 원본에는 영향을 주지 않음
+	// CP949 방식
+	const char* test = "아아아"; // const char*에서 다른 언어가 섞여있으면 2바이트로 동작
+	cout << test << endl;
 
-	// 3) 추가
-	str.append("1234");
-	str += "1234";
+	// UTF-8
+	auto test2 = u8"가나다라마바사";
+	setlocale(LC_ALL, "en_US.UTF-8");
+	cout << test2 << endl;
 
-	// 4) 찾기
-	auto c = str.find("Roo"); // 몇번째 인덱스에 Roo가 있는지 찾음
-	if (c == std::string::npos) // npos가 벡터의 end()와 같음
-	{
-		cout << "not found" << endl;
-	}
+	// UFT-16
+	auto test3 = L"아야어요우유으이";
+	wcout << test3 << endl;
 
-	// 5) 교체
-	string chatStr = "SHIT !!!";
-	string findStr = "SHIT";
-	string replaceStr = "****";
-
-	chatStr.replace(chatStr.find(findStr), findStr.length(), replaceStr);
-
-	string str4 = str.substr(0, 3); // 문자열 자르기
+	// char, string 말고 다른 버전의 문자열 (w = WBCS)
+	wchar_t ch1 = L'루'; // L로 와이드 바이트라는 걸 표현
+	wstring name = L"루이지";
+	wcout << name << endl;
 
 	return 0;
 }
